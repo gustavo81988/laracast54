da39e1f0ee005550ab55e4d8beb712bbba68e69b
Gustavo Ramírez <gustavo81988@gmail.com>
2018-03-11 20:42:13 -0400

    Controllers

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,6 @@
 |
 */
 
-Route::get('/tasks', function () {
-    $tasks = App\Task::all();
-    return view('tasks.index', compact('tasks') );
-});
-
-Route::get('/tasks/{task}', function ($id) {
-    $task = DB::table('tasks')->find($id);
-    return  view('tasks.show', compact('task') );
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
 
