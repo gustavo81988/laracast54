166e0643384f0752564f312465b8a4fe8319b6b6
Gustavo Ramírez <gustavo81988@gmail.com>
2018-03-13 00:02:59 -0400

    Form Request Data and CSRF

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,32 +1,36 @@
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-
-    <title>Album example for Bootstrap</title>
+    <title>Blog Template for Bootstrap</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/blog/">
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="css/album.css" rel="stylesheet">
+    <link href="<?= asset('css/blog.css')?>" rel="stylesheet">
   </head>
-
   <body>
+      
+    @include('layouts.nav')
 
-    <header>
-        @include('layouts.nav')
-    </header>
-    
-    <div class="container">
-        @yield('content')
+    <div class="blog-header">
+      <div class="container">
+        <h1 class="blog-title">The Bootstrap Blog</h1>
+        <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+      </div>
     </div>
 
+    <div class="container">
+      <div class="row">
+        @yield('content')
+        @include('layouts.sidebar')
+      </div><!-- /.row -->
+    </div><!-- /.container -->
+ 
     @include('layouts.footer')
 
   </body>
--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,6 @@
 */
 
 Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/create', 'PostsController@create');
+Route::post('/posts/create', 'PostsController@store');
+//Route::get('/posts/{post}', 'PostsController@show');
